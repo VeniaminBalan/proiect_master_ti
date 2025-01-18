1264c293bf9ac78850ffb545cbe806c159087dd2
Veniamin <veniamin.balan02@gmail.com>
2025-01-18 23:45:10 +0200

    wip

---
+++ MVC/Controllers/TaxeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
+using MVC.Models;
+using MVC.ValueObjects;
+
+namespace MVC.Controllers;
+
+[Route("ModificareProcente")]
+public class TaxeController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    
+    public TaxeController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var taxe = await _context.Taxe.FirstOrDefaultAsync();
+        
+        return View(taxe);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Index(UpdateTaxeViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return RedirectToAction("Index");
+        
+        var taxe = _context.Taxe.FirstOrDefault();
+        
+        if(taxe == null)
+            return RedirectToAction("Index");
+        
+        taxe.CAS = new Percentage(model.CAS);
+        taxe.CASS = new Percentage(model.CASS);
+        taxe.Impozit = new Percentage(model.Impozit);
+            
+        _context.SaveChanges();
+        
+        return View(taxe);
+    }
+}
---
+++ MVC/Models/UpdateTaxeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models;
+
+public class UpdateTaxeViewModel
+{
+    [Range(0, 100, ErrorMessage = "CAS trebuie să fie între 0% și 100%.")]
+    public int CAS { get; set; }
+    [Range(0, 100, ErrorMessage = "CASS trebuie să fie între 0% și 100%.")]
+    public int CASS { get; set; }
+    [Range(0, 100, ErrorMessage = "Impozitul trebuie să fie între 0% și 100%.")]
+    public int Impozit { get; set; }
+}
